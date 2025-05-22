6d47b292d42f83e866df9c5acc11d7cd09e522fb
Adam Ahmed <sdi2400225@di.uoa.gr>
2025-05-22 23:40:58 +0300

    saif 5wl

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import 'aos/dist/aos.css';
 import AOS from 'aos';
+import WhatsAppButton from './components/WhatsAppButton';
 
 const queryClient = new QueryClient();
 
@@ -26,6 +27,7 @@ const App = () => {
             <Route path="*" element={<NotFound />} />
           </Routes>
         </BrowserRouter>
+        <WhatsAppButton />
       </TooltipProvider>
     </QueryClientProvider>
   );
