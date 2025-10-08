a85afb9712235c9fd1d31fc60fb4a88875553d6c
Adam Ahmed <sdi2400225@di.uoa.gr>
2025-10-08 16:01:12 +0300

    m

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -3,15 +3,12 @@ import HeroSection from '@/components/HeroSection';
 import ServicesSection from '@/components/ServicesSection';
 import NewServicesSection from '@/components/NewServicesSection';
 import ComparisonSection from '@/components/ComparisonSection';
-import AboutSection from '@/components/AboutSection';
-import TeamSection from '@/components/TeamSection';
-import TestimonialsSection from '@/components/TestimonialsSection';
-import ContactSection from '@/components/ContactSection';
+
 import NewContactSection from '@/components/NewContactSection';
 import BookingSection from '@/components/BookingSection';
 import ChatbotSection from '@/components/ChatbotSection';
 import Footer from '@/components/Footer';
-import { FAQSection } from '@/components/FAQSection';
+
 
 const Index = () => {
   return (
